90d6ff8d0755f26b2847df761936db64b4724234
tsuiusi <ryantsui786@gmail.com>
2024-07-26 18:23:05 +0800

    inital commit

---
+++ templates/mlx.py
@@ -0,0 +1,13 @@
+import mlx
+import mlx.nn as nn
+import mlx.core as mx
+import mlx.optimizers as optim
+
+import os
+import sys
+import pickle
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+
---
+++ templates/torch.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+import os
+import sys
+import pickle
+import time
+
+import numpy as np
+import matplotlib.pyplot as plt
+
