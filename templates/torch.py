acd4dc69a69b90803d473fe77b3a12f9ea354400
tsuiusi <ryantsui786@gmail.com>
2025-04-02 13:58:44 +0100

    update dotfiles

--- templates/torch.py
+++ templates/torch.py
@@ -4,6 +4,9 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset
 
+from dataclasses import dataclass
+from einops import rearrange, repeat, einsum
+from typing import Tuple
+
 import math
 import numpy as np
-
